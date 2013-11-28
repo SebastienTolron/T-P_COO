2400dcb36cc0c4e774e60846293182392c16df98
Sebastien Tolron <sebastien.tolron@gmail.com>
2013-11-28 09:35:15 +0100

    debut de larbre

--- src/com/tosviel/coo/tpent/ihm/FenetreAjoutObjet.java
+++ src/com/tosviel/coo/tpent/ihm/FenetreAjoutObjet.java
@@ -4,27 +4,31 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import javax.swing.JTree;
 
 
 
 
 
-public class FenetreAjoutObjet extends JFrame  {
+public class FenetreAjoutObjet extends JDialog  {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	public String ObjetInfo = " "  ;
 
 	
 	public FenetreAjoutObjet()  {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -48,7 +52,7 @@ public class FenetreAjoutObjet extends JFrame  {
 		lblType.setBounds(10, 135, 55, 14);
 		contentPane.add(lblType);
 		
-		JComboBox comboBox = new JComboBox();
+		final JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(76, 132, 86, 20);
 		contentPane.add(comboBox);
 		comboBox.addItem("Dossier");
@@ -58,16 +62,30 @@ public class FenetreAjoutObjet extends JFrame  {
 		btnValider.setBounds(159, 228, 89, 23);
 		contentPane.add(btnValider);
 		
+		JTree tree = new JTree();
+		tree.setBounds(209, 71, 190, 119);
+		contentPane.add(tree);
+		
 		btnValider.addActionListener(new ActionListener(){
 		      
 			public void actionPerformed(ActionEvent e){
 	        	
-				System.out.println("youhou");
-				setVisible(false);
+				setObjetInfo(textField.getText()+";;"+comboBox.getSelectedItem().toString());
+				dispose();
 				
 	            }
 	 });
 		
 		
+	}
+
+
+	public String getObjetInfo() {
+		return ObjetInfo;
+	}
+
+
+	public void setObjetInfo(String objetInfo) {
+		ObjetInfo = objetInfo;
 	}
 }
